32ecc02eba13ac6d58a013d22cd61739cc358efa
KennedyB1 <114904314+KennedyB1@users.noreply.github.com>
2023-11-20 12:32:52 +0100

    Update GameSearch.tsx

--- steam/src/GameSearch.tsx
+++ steam/src/GameSearch.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 interface Game {
@@ -9,40 +9,29 @@ interface Game {
 const GameSearch: React.FC = () => {
   const [inputValue, setInputValue] = useState<string>('');
   const [games, setGames] = useState<Game[]>([]);
-  const [isSearching, setIsSearching] = useState<boolean>(false);
 
   const fetchGames = async (searchTerm: string) => {
-    setIsSearching(true);
+    if (!searchTerm.trim()) {
+      setGames([]);
+      return;
+    }
+
     try {
       const response = await axios.get(`/api/steam?searchTerm=${encodeURIComponent(searchTerm)}`);
       setGames(response.data.applist.apps);
     } catch (error) {
       console.error('Error fetching data:', error);
-    } finally {
-      setIsSearching(false);
     }
   };
 
-  useEffect(() => {
-    const delayDebounce = setTimeout(() => {
-      if (inputValue.trim()) {
-        fetchGames(inputValue);
-      } else {
-        setGames([]);
-      }
-    }, 500); // 500 ms delay
-
-    return () => clearTimeout(delayDebounce);
-  }, [inputValue]);
-
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(event.target.value);
+    fetchGames(event.target.value);
   };
 
   return (
     <div>
       <input type="text" value={inputValue} onChange={handleInputChange} />
-      {isSearching && <div>Searching...</div>}
       <ul>
         {games.map(game => (
           <li key={game.appid}>{game.name}</li>
